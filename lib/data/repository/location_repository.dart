113c43888d890b0012b239f68b0b5f12d604f3a2
imeujin <imeujin.dev@gmail.com>
2024-12-06 10:55:02 +0900

    [fix] 예외처리 추가

--- lib/data/repository/location_repository.dart
+++ lib/data/repository/location_repository.dart
@@ -17,22 +17,27 @@ class LocationRepository {
 
   /// 지역정보 검색한 데이터 요청
   Future<List<Location>?> findByAddress(String query) async {
-    // api 에 파라미터 입력해서 응답 값 받기
-    final response = await client.get(
-      'https://openapi.naver.com/v1/search/local.json',
-      queryParameters: {
-        'query': query,
-        'display': 5,
-      },
-    );
+    try {
+      // api 에 파라미터 입력해서 응답 값 받기
+      final response = await client.get(
+        'https://openapi.naver.com/v1/search/local.json',
+        queryParameters: {
+          'query': query,
+          'display': 5,
+        },
+      );
 
-    // 요청 성공
-    if (response.statusCode == 200) {
-      // 리스트로 변환
-      final items = List.from(response.data['items']);
-      return items.map((item) {
-        return Location.fromJson(item);
-      }).toList();
+      // 요청 성공
+      if (response.statusCode == 200) {
+        // 리스트로 변환
+        final items = List.from(response.data['items']);
+        return items.map((item) {
+          return Location.fromJson(item);
+        }).toList();
+      }
+    } catch (e) {
+      print(e);
+      return null;
     }
 
     // 실패하면 null
--- lib/data/repository/vworld_repository.dart
+++ lib/data/repository/vworld_repository.dart
@@ -7,28 +7,34 @@ class VworldRepository {
     validateStatus: (status) => true,
   ));
 
+  /// 위도 경도 정보로 위치 찾기
   Future<List<String>> findByLatLng(double lat, double lng) async {
-    final response = await client.get(
-      'https://api.vworld.kr/req/data',
-      queryParameters: {
-        'request': 'GetFeature',
-        'key': key,
-        'data': 'LT_C_ADEMD_INFO',
-        'geomFilter': 'POINT($lng $lat)',
-        'geometry': false,
-        'size': 100,
-      },
-    );
+    try {
+      final response = await client.get(
+        'https://api.vworld.kr/req/data',
+        queryParameters: {
+          'request': 'GetFeature',
+          'key': key,
+          'data': 'LT_C_ADEMD_INFO',
+          'geomFilter': 'POINT($lng $lat)',
+          'geometry': false,
+          'size': 100,
+        },
+      );
 
-    // response > result > featureCollection > features >> properties > full_nm
-    if (response.statusCode == 200 &&
-        response.data['response']['status'] == 'OK') {
-      final features =
-          response.data['response']['result']['featureCollection']['features'];
+      // response > result > featureCollection > features >> properties > full_nm
+      if (response.statusCode == 200 &&
+          response.data['response']['status'] == 'OK') {
+        final features = response.data['response']['result']
+            ['featureCollection']['features'];
 
-      return List.from(features).map((feat) {
-        return '${feat['properties']['full_nm']}';
-      }).toList();
+        return List.from(features).map((feat) {
+          return '${feat['properties']['full_nm']}';
+        }).toList();
+      }
+    } catch (e) {
+      print(e);
+      return [];
     }
 
     return [];
